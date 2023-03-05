049aec20cc2c779628aabbc55b78b9a999a40064
Isaac_L <dizack10@gmail.com>
2023-03-05 21:49:06 +0100

    Rajout de projets

--- ThreeJS/perso.js
+++ ThreeJS/perso.js
@@ -8,6 +8,9 @@ import {
     scene,
 } from './post.js'
 
+let mouse = new THREE.Vector2();
+let raycaster = new THREE.Raycaster();
+
 export default function loaderPerso() {
     const assetLoader = new GLTFLoader();
 
@@ -57,7 +60,55 @@ export default function loaderPerso() {
                 requestAnimationFrame(animate);
             }
         }
-
+/*
+        function onClick(event){
+            event.preventDefault();
+        
+            mouse.x = (event.offsetX / window.innerWidth) * 2 - 1;
+            mouse.y = -(event.offsetY / window.innerHeight) * 2 + 1;
+            console.log("X : ", mouse.x);
+            console.log("Y : ", mouse.y);
+        
+            raycaster.setFromCamera(mouse, camera);
+        
+            var intersects = raycaster.intersectObjects(scene.children);
+        
+            const Perso = intersects.find(intersect => intersect.object.name === "Mountain_01");
+            console.dir(intersects);
+        
+            if(Perso){
+                helloAnimation();
+        
+                function helloAnimation() {
+                    const character = model.children[0];
+                    const clips = gltf.animations;
+        
+                    const helloClip = clips.find(clip => clip.name.includes("Hello"));
+                    character.animations.push(helloClip);
+        
+                    //console.log(character);
+        
+                    const mixer = new THREE.AnimationMixer(character);
+        
+                    const action = mixer.clipAction(helloClip)
+                    action.play();
+        
+                    var clock = new THREE.Clock();
+        
+                    animate();
+        
+                    function animate() {
+                        var dt = clock.getDelta();
+                        mixer.update(dt);
+                        requestAnimationFrame(animate);
+                    }
+                }
+            } else {
+                console.error("no clickable object");
+            };
+        }
+        window.addEventListener('click', onClick, false);
+*/
 
     }, undefined, function (error) {
         console.error(error);
